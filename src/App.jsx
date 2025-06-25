087618912ec0e4df701c76f2638d8c9dea544ec6
franciscoDastres <franciscomeza.1234@gmail.com>
2025-06-25 02:52:44 -0400

    fix: :bug: fixing routes using new components not found

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,43 @@
+import { Routes, Route } from "react-router-dom"
 import TopBanner from "./components/TopBanner/TopBanner"
 import Header from "./components/Header/Header"
 import HeroSection from "./components/HeroSection/HeroSection"
 import CategoryCards from "./components/CategoryCards/CategoryCards"
 import PopularProducts from "./components/PopularProducts/PopularProducts"
+import NotFound from "./components/NotFound/NotFound.jsx"
 
-function App() {
+import "./index.css"
+
+// Puedes crear un Layout si todos los componentes están en todas las páginas
+function Layout() {
   return (
-    <div className="min-h-screen bg-gray-50">
+    <>
       <TopBanner />
       <Header />
+    </>
+  )
+}
+
+function HomePage() {
+  return (
+    <>
       <HeroSection />
       <CategoryCards />
       <PopularProducts />
+    </>
+  )
+}
+
+// Aquí podrías agregar otras páginas en el futuro como /women o /men
+function App() {
+  return (
+    <div className="w-screen min-h-screen bg-gray-100">
+      <Layout />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        {/* Rutas adicionales */}
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </div>
   )
 }
